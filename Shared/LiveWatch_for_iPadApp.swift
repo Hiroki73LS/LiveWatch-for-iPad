0d0b261ca526dfa82c433d1cd1464c85ff113624
Hiroki mac air <aichan_lovepower@yahoo.co.jp>
2022-11-20 19:29:23 +0900

    Initial Commit

---
+++ Shared/LiveWatch_for_iPadApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiveWatch_for_iPadApp.swift
+//  Shared
+//
+//  Created by 林宏樹 on 2022/11/20.
+//
+
+import SwiftUI
+
+@main
+struct LiveWatch_for_iPadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
